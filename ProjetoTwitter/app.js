10b4730997f930aae37a73ff9ab899799e00f653
Willyanne Silva <110118532+maiiyx@users.noreply.github.com>
2023-05-08 18:26:26 -0300

    Add files via upload

---
+++ ProjetoTwitter/app.js
@@ -0,0 +1,8 @@
+// document.querySelector('#heart').addEventListener('click',()=>{
+//     document.querySelector('#heart').style.display = 'none'
+//     document.querySelector('#heart').style.display = 'block'
+// })
+// document.querySelector('#heart').addEventListener('click',()=>{
+//     document.querySelector('#heart').style.display = 'block'
+//     document.querySelector('#heart').style.display = 'none'
+// })
